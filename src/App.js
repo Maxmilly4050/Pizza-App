a8bcdf4c86d3280ba8d301a455fc2066a04a70da
Maxmilly4050 <maxsteel4050@gmail.com>
2026-04-28 21:47:17 +0300

    Add Order component to display operating hours and order button

--- src/App.js
+++ src/App.js
@@ -107,6 +107,18 @@ function Footer() {
   // return React.createElement("footer", null, "We're currently open!");
 }
 
+function Order({ closeHour, openHour }) {
+  return (
+      <div className="order">
+        <p>
+          We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
+          online.
+        </p>
+        <button className="btn">Order</button>
+      </div>
+  );
+}
+
 
 function App() {
   return (
