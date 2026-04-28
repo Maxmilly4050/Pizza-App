51ca81b1fc2b19239d99902394f2fa218a117972
Maxmilly4050 <maxsteel4050@gmail.com>
2026-04-28 21:45:24 +0300

    Add Menu component to display pizza options from JSON data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React from 'react';
+import pizzaData from '../public/data';
 
 
 function Header() {
@@ -10,6 +11,72 @@ function Header() {
       <header className="header">
         <h1 style={style}>Fast React Pizza Co.</h1>
       </header>
+  );
+}
+
+function Menu() {
+  const pizzas = pizzaData;
+  // const pizzas = [];
+  const numPizzas = pizzas.length;
+
+  return (
+      <main className="menu">
+        <h2>Our menu</h2>
+
+        {numPizzas > 0 ? (
+            <>
+              <p>
+                Authentic Italian cuisine. 6 creative dishes to choose from. All
+                from our stone oven, all organic, all delicious.
+              </p>
+
+              <ul className="pizzas">
+                {pizzas.map((pizza) => (
+                    <Pizza pizzaObj={pizza} key={pizza.name} />
+                ))}
+              </ul>
+            </>
+        ) : (
+            <p>We're still working on our menu. Please come back later :)</p>
+        )}
+
+        {/* <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mushrooms"
+        price={12}
+        photoName="pizzas/funghi.jpg"
+      /> */}
+      </main>
+  );
+}
+
+function Pizza({ pizzaObj }) {
+  console.log(pizzaObj);
+
+  // if (pizzaObj.soldOut) return null;
+
+  return (
+      <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+        <img src={pizzaObj.photoName} alt={pizzaObj.name} />
+        <div>
+          <h3>{pizzaObj.name}</h3>
+          <p>{pizzaObj.ingredients}</p>
+
+          {/* {pizzaObj.soldOut ? (
+          <span>SOLD OUT</span>
+        ) : (
+          <span>{pizzaObj.price}</span>
+        )} */}
+
+          <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
+        </div>
+      </li>
   );
 }
 
